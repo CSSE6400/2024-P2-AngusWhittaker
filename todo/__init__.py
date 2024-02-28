2f16651671f611e22a478c8c91e8c926a0b3c438
AngusWhittaker <137162337+AngusWhittaker@users.noreply.github.com>
2024-02-28 06:28:19 +0000

    commit all

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,18 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+# Cmd to run server: poetry run flask --app todo run -p 6400 --debug 
+#Cmd to inspect database: sqlite3 instance/db.sqlite
+
 def create_app():
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite"
+    
+    if config_overrides: 
+      app.config.update(config_overrides)
 
     # Load the models 
-   from todo.models import db 
-   from todo.models.todo import Todo 
-   db.init_app(app) 
+    from todo.models import db 
+    from todo.models.todo import Todo 
+    db.init_app(app) 
 
-   # Create the database tables 
-   with app.app_context(): 
+    # Create the database tables 
+    with app.app_context(): 
       db.create_all() 
       db.session.commit() 
 
